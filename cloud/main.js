e7ac28dd4f1ad9896d271c616b7edb52cbee901c
pabeekm <patricia.beekmanjunk@gmail.com>
2016-07-13 13:44:44 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,7 +8,7 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
 
 
   // use to custom tweak whatever payload you wish to send
-  var pushQuery = new Parse.Query(Parse.Installation);
+  var pushQuery = new Parse.Query("_Installation");
   pushQuery.equalTo("deviceType", "android");
 
   var payload = { "alert": message };
