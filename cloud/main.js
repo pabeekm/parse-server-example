34b50e35c929da1d44689616b41a86afa3ae1c51
pabeekm <patricia.beekmanjunk@gmail.com>
2016-07-17 23:24:29 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -27,7 +27,7 @@ Parse.Cloud.define('spamAllUsers', function(request, response) {
 });
 
 // Push to all users in range
-Parse.Cloud.define('spamAllUsersInRange', function(request, response) {
+/*Parse.Cloud.define('spamAllUsersInRange', function(request, response) {
   
   var params = request.params;
   var user = request.user;
@@ -61,7 +61,7 @@ Parse.Cloud.define('spamAllUsersInRange', function(request, response) {
   }, useMasterKey: true});
 
   response.success('success');
-});
+});*/
 
 // Convert a string "lat, lang", into a GeoPoint object and assign it to the user.
 Parse.Cloud.define('assignGeoPoint', function(request, response) {
