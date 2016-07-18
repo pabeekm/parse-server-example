0934d5cbf81944dbf437f5e1d185b964eda2bdd5
pabeekm <patricia.beekmanjunk@gmail.com>
2016-07-17 21:57:20 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -72,5 +72,5 @@ Parse.Cloud.define('assignGeoPoint', function(request, response) {
   var point = new Parse.GeoPoint(lat, lon);
   
   user.set("geoPoint", point);
-  user.save();
+  user.save(null, {useMasterKey:true});
 });
