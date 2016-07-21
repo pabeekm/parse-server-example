6130b15567273e0d75671d6cc4e174da5d6472af
pabeekm <patricia.beekmanjunk@gmail.com>
2016-07-21 13:50:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
+var kue = require('kue');
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 
