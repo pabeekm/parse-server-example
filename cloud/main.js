9ac2e8468627b10192a7e0fe5075845179e7144e
pabeekm <patricia.beekmanjunk@gmail.com>
2016-07-26 19:48:06 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -13,7 +13,8 @@ Parse.Cloud.define('spamAllUsers', function(request, response) {
   var userQuery = new Parse.Query(Parse.User);
   userQuery.containedIn("FBid", request.user.get("friendsIDs"));
   var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.matchesQuery("user", userQuery);
+  //pushQuery.matchesQuery("user", userQuery);
+  pushQuery.equals("deviceType", "android");
   Parse.Push.send({
   where: pushQuery,     
   data: {
